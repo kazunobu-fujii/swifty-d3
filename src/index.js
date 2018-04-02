dc539f3b50bde735a177e1e8ec78291157abb248
FUJII Kazunobu <fujii.kazunobu@gmail.com>
2018-04-02 13:14:08 +0900

    support D3.js v5.x

--- src/index.js
+++ src/index.js
@@ -1,21 +1,19 @@
-import d3Scale from 'd3-scale';
-import d3Shape from 'd3-shape';
-import d3Array from 'd3-array';
+import * as d3 from 'd3';
 
 export let getPath = (data, dim) => { 
   const [w, h] = dim; 
-  const x = d3Scale.scaleLinear()
+  const x = d3.scaleLinear()
     .domain([0, data.length])
     .range([0, w]);
   
-  const y = d3Scale.scaleLinear()
-    .domain([0, d3Array.max(data)])
+  const y = d3.scaleLinear()
+    .domain([0, d3.max(data)])
     .range([0, h]);
     
-  const line = d3Shape.line()
+  const line = d3.line()
     .x((d, i) =>  x(i))
     .y((d, i) => y(d))
-    .curve(d3Shape.curveBasis);
+    .curve(d3.curveBasis);
   
   return line(data);
 }
